aef93514f7bc2d878b9ffdf94bf32fac9aa0102b
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2020-11-05 20:47:32 -0400

    Agregar PublicationApi y PublicationBl

---
+++ src/main/java/bo/ucb/edu/ingsoft/api/PublicationApi.java
@@ -0,0 +1,9 @@
+package bo.ucb.edu.ingsoft.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/list")
+public class PublicationApi {
+}
