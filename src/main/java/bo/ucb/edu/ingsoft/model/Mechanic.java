1668049168053d6cfd79024078ae043895a03e97
Joselian2204 <61953806+Joselian2204@users.noreply.github.com>
2020-11-05 20:36:42 -0400

    agregando modelos City,Mechanic y HMechanic

---
+++ src/main/java/bo/ucb/edu/ingsoft/model/Mechanic.java
@@ -0,0 +1,105 @@
+package bo.ucb.edu.ingsoft.model;
+
+public class Mechanic {
+    private Integer idMechanic;
+    private Integer idPerson;
+    private Integer idCity;
+    private String location;
+    private String shopName;
+    private Integer stars;
+    private Integer starsCount;
+    private Integer status;
+    private Transaction transaction;
+
+    public Mechanic(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public Integer getIdMechanic() {
+        return idMechanic;
+    }
+
+    public void setIdMechanic(Integer idMechanic) {
+        this.idMechanic = idMechanic;
+    }
+
+    public Integer getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(Integer idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public Integer getIdCity() {
+        return idCity;
+    }
+
+    public void setIdCity(Integer idCity) {
+        this.idCity = idCity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public Integer getStars() {
+        return stars;
+    }
+
+    public void setStars(Integer stars) {
+        this.stars = stars;
+    }
+
+    public Integer getStarsCount() {
+        return starsCount;
+    }
+
+    public void setStarsCount(Integer starsCount) {
+        this.starsCount = starsCount;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    @Override
+    public String
+    toString() {
+        return "Mechanic{" +
+                "idMechanic=" + idMechanic +
+                ", idPerson=" + idPerson +
+                ", idCity=" + idCity +
+                ", location='" + location + '\'' +
+                ", shopName='" + shopName + '\'' +
+                ", stars=" + stars +
+                ", starsCount=" + starsCount +
+                ", status=" + status +
+                ", transaction=" + transaction +
+                '}';
+    }
+}
