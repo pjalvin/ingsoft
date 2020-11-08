43a3a7b9422d4fafc0e864d3a4d920da9d2369be
Silvana <silvana.munoz.vargas@gmail.com>
2020-11-08 18:45:25 -0400

    Correccion trigger after update en user,seller y person

--- inserts_triggers_cabalito.sql
+++ inserts_triggers_cabalito.sql
@@ -1,4 +1,11 @@
 -- End of file.
+drop trigger if exists tg_insert_h_person;
+drop trigger if exists tg_update_h_person;
+drop trigger if exists tg_insert_h_user;
+drop trigger if exists tg_update_h_user;
+drop trigger if exists tg_insert_h_seller;
+drop trigger if exists tg_update_h_seller;
+
 DELIMITER |
 CREATE TRIGGER tg_insert_h_person
 AFTER INSERT ON person
@@ -14,7 +21,7 @@ CREATE TRIGGER tg_update_h_person
 AFTER UPDATE ON person
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_person`(`id_person`,`first_name`,`last_name`,`phone_number`,`status`,`tx_date`,`tx_id_user`,`tx_host`,`tx_update`) VALUES (NULL, NEW.id_person,NEW.first_name,NEW.last_name,NEW.phone_number,NEW.status, NEW.tx_date,NEW.tx_id_user,NEW.tx_host,NEW.tx_update);
+	INSERT INTO `h_person`(`id_person`,`first_name`,`last_name`,`phone_number`,`status`,`tx_date`,`tx_id_user`,`tx_host`,`tx_update`) VALUES (NEW.id_person,NEW.first_name,NEW.last_name,NEW.phone_number,NEW.status, NEW.tx_date,NEW.tx_id_user,NEW.tx_host,NEW.tx_update);
     END;
 |
 DELIMITER ;
@@ -46,7 +53,7 @@ CREATE TRIGGER tg_update_h_user
 AFTER UPDATE ON user
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_user`(`id_user`,`password`,`email`,`status`,`tx_date`,`tx_id_user`,`tx_host`,`tx_update`) VALUES (NULL, NEW.id_user,NEW.password,NEW.email,NEW.status,NEW.tx_date,NEW.tx_id_user,NEW.tx_host,NEW.tx_update);
+	INSERT INTO `h_user`(`id_user`,`password`,`email`,`status`,`tx_date`,`tx_id_user`,`tx_host`,`tx_update`) VALUES (NEW.id_user,NEW.password,NEW.email,NEW.status,NEW.tx_date,NEW.tx_id_user,NEW.tx_host,NEW.tx_update);
 END;
 |
 DELIMITER ;
@@ -73,7 +80,7 @@ CREATE TRIGGER tg_update_h_seller
 AFTER UPDATE ON seller
 FOR EACH ROW
 BEGIN
-	INSERT INTO `h_seller`(`id_seller`,`id_person`,`id_user`,`status`,`image_path`,`tx_date`,`tx_id_user`,`tx_host`,`tx_update`) VALUES (NULL, NEW.id_seller,NEW.id_person,NEW.id_user,NEW.status,NEW.image_path,NEW.tx_date,NEW.tx_id_user,NEW.tx_host,NEW.tx_update);
+	INSERT INTO `h_seller`(`id_seller`,`id_person`,`id_user`,`status`,`image_path`,`tx_date`,`tx_id_user`,`tx_host`,`tx_update`) VALUES (NEW.id_seller,NEW.id_person,NEW.id_user,NEW.status,NEW.image_path,NEW.tx_date,NEW.tx_id_user,NEW.tx_host,NEW.tx_update);
 END;
 |
 DELIMITER ;
